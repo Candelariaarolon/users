0b7df60f534791aaeeec21178f8742204baa50ee
candelariaarolon <2302932@ucc.edu.ar>
2026-04-12 22:39:02 -0300

    fix fatal log

--- backend/main.go
+++ backend/main.go
@@ -15,7 +15,7 @@ func main() {
 	// Load .env file
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	//variable que me apunta al llamado
